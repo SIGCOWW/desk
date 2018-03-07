b600d19e3a7404bbee1d27ade15eb6e187f181bc
lrks <yuke1222@gmail.com>
2018-03-07 18:21:42 +0900

    privateなユーザの鍵も取ってくるようにした

--- remote/keys.sh
+++ remote/keys.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 ORG="SIGCOWW"
-USER="sigcoww"
+SERVER_USER="sigcoww"
+TOKEN="$1"
 
-file="/home/${USER}/.ssh/authorized_keys"
+file="/home/${SERVER_USER}/.ssh/authorized_keys"
 rm -f $file
-curl -s "https://api.github.com/orgs/${ORG}/members" | jq -r '.[] | select(.type == "User") | .login' | while read -r user; do
+
+uri="https://api.github.com/orgs/${ORG}/members"
+if [ "$TOKEN" != "" ]; then uri="${uri}?access_token=${TOKEN}"; fi
+curl -s "$uri" | jq -r '.[] | select(.type == "User") | .login' | while read -r user; do
 	sleep 1
 	curl -s "https://api.github.com/users/${user}/keys" | jq -r '.[] | .key' | while read -r key; do
-		line="command=\"build.sh\",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding ${key}"
+		line="command=\"/home/${SERVER_USER}/.ssh/allowed-commands.sh\",no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding ${key}"
 		echo "$line" >> $file
 	done
 done
 
 chmod 600 $file
-chown "${USER}:${USER}" $file
+chown "${SERVER_USER}:${SERVER_USER}" $file
