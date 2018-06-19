e0d939f167fe015debfe0147c9e82e81a87f64f3
lrks <yuke1222@gmail.com>
2018-06-20 00:22:03 +0900

    軽い組版調整

--- docker/extensions/okuduke.rb
+++ docker/extensions/okuduke.rb
@@ -29,7 +29,7 @@ module ReVIEW
       rows << ['', `review-pdfmaker --version`]
       rows << ['', `uplatex --version | head -n1`]
       rows << ['', "Alpine Linux #{`cat /etc/alpine-release`.strip} (Linux Kernel #{`uname -r`.strip})"]
-      rows << ['', "SIGCOWW/desk https://github.com/SIGCOWW/desk"]
+      rows << ['', "desk ←←←←← 熱烈歓迎Contribution https://github.com/SIGCOWW/desk"]
 
       ret = ''
       rows.each do | r |
