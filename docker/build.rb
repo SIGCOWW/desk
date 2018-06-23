e079d971f46e0d98dc9e0af3bc4d74ff0c8f833c
lrks <yuke1222@gmail.com>
2018-06-23 16:10:00 +0900

    動作検証

--- docker/build.rb
+++ docker/build.rb
@@ -201,8 +201,8 @@ eof
     header("Preprocessing", 2)
     articles = {}
     newcatalog = {}
-    run('cp -Rn /extensions/*.* ./')
-    run("cp -Rn #{['config.yml', 'layouts/', 'cover.png', 'back.png'].map{|v| "../#{v}"}.join(' ')} ./")
+    run('yes n | cp -Ri /extensions/*.* ./')
+    run("yes n | cp -Ri #{['config.yml', 'layouts/', 'cover.png', 'back.png'].map{|v| "../#{v}"}.join(' ')} ./")
     FileUtils.mv(['locale.yml', 'style.css'].map{|v| "layouts/#{v}"}, './', {:force => true})
     FileUtils.mkdir_p('sty')
     FileUtils.mv(Dir.glob('layouts/*.sty'), 'sty', {:force => true})
