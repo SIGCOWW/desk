9805dba1e5ef57a7c99207eba4195086429e055b
lrks <yuke1222@gmail.com>
2018-11-10 01:40:13 +0900

    QR出力のLaTeXマクロ化

--- docker/extensions/qrcode.rb
+++ docker/extensions/qrcode.rb
@@ -11,12 +11,7 @@ module ReVIEW
   class LATEXBuilder
     def inline_qrcode(str)
       str.gsub!(/[\\\{\}]/) { |s| '\\' + s }
-      ret = '\vspace{0.5\baselineskip}\\\\{'
-      ret += '\setlength\lineskiplimit{0pt}'
-      ret += '\setlength\normallineskiplimit{0pt}'
-      ret += "\\qrcode[tight,height=\\marginparwidth]{#{str}}"
-      ret += '}\vspace{0.5\baselineskip}\\\\'
-      return ret
+      return "\\reviewqr{#{str}}"
     end
   end
 end
